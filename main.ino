169144d71be6ba3bfba3b41ad30d87d0424483ec
rukkro <rukkro@users.noreply.github.com>
2018-12-22 15:55:15 -0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,12 @@
+// Libraries
 #include <Adafruit_NeoPixel.h>
 #include <Keyboard.h>
 
-// Pins used
+// Pins used on Pro Micro
 #define LED_PIN 5
 #define BUTTON_PIN 2
 
-// Keyboard stuff from https://www.sparkfun.com/tutorials/337
+// Special keyboard keys: https://www.sparkfun.com/tutorials/337
 #define KEY_LEFT_CTRL  0x80
 #define KEY_LEFT_SHIFT 0x81
 #define KEY_LEFT_ALT   0x82
@@ -38,16 +39,17 @@
 #define KEY_F7     0xC8
 #define KEY_F8     0xC9
 #define KEY_F9     0xCA
-#define KEY_F10        0xCB
-#define KEY_F11        0xCC
-#define KEY_F12        0xCD
+#define KEY_F10    0xCB
+#define KEY_F11    0xCC
+#define KEY_F12    0xCD
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(160, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Off color for LEDs
-#define OFF strip.Color(0, 0, 0)
+#define OFF strip.Color(0, 0, 0) // Off color for LEDs
+#define PRESS_THRESHOLD_MIN 100 // Approx min # of cycles button held to trigger action
+#define PRESS_HOLD_THRESHOLD 10000 // Approx # of cycles button held down before considered a button hold action
 
-// Coloring stuff from https://arduino.stackexchange.com/a/35769
+// For finding RGB values: https://arduino.stackexchange.com/a/35769
 const uint8_t lights[360] = {
   0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 15, 17, 18, 20, 22, 24, 26, 28, 30, 32, 35, 37, 39,
   42, 44, 47, 49, 52, 55, 58, 60, 63, 66, 69, 72, 75, 78, 81, 85, 88, 91, 94, 97, 101, 104, 107, 111, 114, 117, 121, 124, 127, 131, 134, 137,
@@ -62,48 +64,62 @@ const uint8_t lights[360] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-int buttonState = 0;
-float downCount = 0;
-bool stateChange = false;
-int r = 0;
-int g = 0;
-int b = 0;
-int rgb_stepper = 0;
+int buttonState = 0; // HIGH, LOW
+float downCount = 0; // Increases as button is pushed down and held
+bool wasPressed = false; // Changed when button was pushed
+int rgb_stepper = 0; // Incremented when rgb values are to change
 
+// Initialization...
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Set baud to 9600
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  strip.begin();
+  strip.begin(); // Initialize NeoPixel ring
   strip.setBrightness(100);
 }
 
+/* 
+  The buttonState is HIGH when:
+    1. The button is not being pressed
+    2. During the action of the button being pressed
+    3. During the action of releasing a button press
+   The buttonState is LOW when:
+    1. The button is pressed down.
+   During a press & hold action, it generally appears to do HIGHs --> LOW --> HIGH --> LOWs
+    - The PRESS_THRESHOLD_MIN prevents the keypress actions during a hold when the second HIGH occurs.
+    During a short press and release action, it appears to go HIGHs --> some LOWs and/or HIGHs --> LOWs --> HIGHs
+    - It should do enough LOWs to hit the PRESS_THRESHOLD_MIN and trigger the keyboard/LED actions.
+*/
 void loop() {
-  buttonState = digitalRead(2);
+  buttonState = digitalRead(2); // Get button state
   Serial.print(buttonState);
-  if (buttonState == HIGH && (stateChange == true)) { // IF NOT DOWN
-    if (downCount <= 10000 && downCount > 0) {
+  
+  if (buttonState == HIGH && wasPressed == true) {
+    // If button is pushed for a shorter duration of time
+    if (downCount <= PRESS_HOLD_THRESHOLD && downCount >= PRESS_THRESHOLD_MIN) {
       setAllColor(strip.Color(lights[(rgb_stepper + 120) % 360], lights[rgb_stepper], lights[(rgb_stepper + 240) % 360]));
-      Keyboard.write(KEY_LEFT_GUI);
-      delay(250);
-      setAllColor(OFF);
+      Keyboard.write(KEY_LEFT_GUI); // Send Windows/Super/Meta keystroke back to PC
+      delay(250); // Short delay to keep LEDs on
+      setAllColor(OFF); // Shut off LEDs
     }
-    stateChange = false;
+    wasPressed = false;
     downCount = 0;
   }
-  else if (buttonState == LOW) { // IF DOWN
-    downCount += 1;
-    if (downCount >= 10000) {
-      delay(10);
+  else if (buttonState == LOW) { 
+    downCount += 1; // Increments every cycle button is down
+    // If button is held down long enough
+    if (downCount >= PRESS_HOLD_THRESHOLD) {
+      delay(10); // Slow down rate at which fade through colors
       rgb_stepper += 1;
       setAllColor(strip.Color(lights[(rgb_stepper + 120) % 360], lights[rgb_stepper], lights[(rgb_stepper + 240) % 360]));
       if (rgb_stepper == 360) {
         rgb_stepper = 0;
       }
     }
-    stateChange = true;
+    wasPressed = true;
   }
 }
 
+// Sets color for all LEDs on NeoPixel ring.
 void setAllColor(uint32_t c) {
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
