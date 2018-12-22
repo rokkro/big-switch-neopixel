1575a8a154753332e7c036a1d5b7321b92dc2c62
rukkro <rukkro@users.noreply.github.com>
2018-12-22 09:48:56 -0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,78 +1,112 @@
 #include <Adafruit_NeoPixel.h>
+#include <Keyboard.h>
 
-#define LED_PIN 5 
+// Pins used
+#define LED_PIN 5
 #define BUTTON_PIN 2
+
+// Keyboard stuff from https://www.sparkfun.com/tutorials/337
+#define KEY_LEFT_CTRL  0x80
+#define KEY_LEFT_SHIFT 0x81
+#define KEY_LEFT_ALT   0x82
+#define KEY_LEFT_GUI   0x83
+#define KEY_RIGHT_CTRL 0x84
+#define KEY_RIGHT_SHIFT    0x85
+#define KEY_RIGHT_ALT  0x86
+#define KEY_RIGHT_GUI  0x87
+#define KEY_UP_ARROW   0xDA
+#define KEY_DOWN_ARROW 0xD9
+#define KEY_LEFT_ARROW 0xD8
+#define KEY_RIGHT_ARROW    0xD7
+#define KEY_BACKSPACE  0xB2
+#define KEY_TAB        0xB3
+#define KEY_RETURN 0xB0
+#define KEY_ESC        0xB1
+#define KEY_INSERT 0xD1
+#define KEY_DELETE 0xD4
+#define KEY_PAGE_UP    0xD3
+#define KEY_PAGE_DOWN  0xD6
+#define KEY_HOME   0xD2
+#define KEY_END        0xD5
+#define KEY_CAPS_LOCK  0xC1
+#define KEY_F1     0xC2
+#define KEY_F2     0xC3
+#define KEY_F3     0xC4
+#define KEY_F4     0xC5
+#define KEY_F5     0xC6
+#define KEY_F6     0xC7
+#define KEY_F7     0xC8
+#define KEY_F8     0xC9
+#define KEY_F9     0xCA
+#define KEY_F10        0xCB
+#define KEY_F11        0xCC
+#define KEY_F12        0xCD
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(160, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define RED strip.Color(255, 0, 0)
-#define ORANGE strip.Color(255, 165, 0)
-#define YELLOW strip.Color(255, 255, 0)
-#define GREEN strip.Color(173, 255, 47)
-#define BLUE strip.Color(0, 191, 255)
-#define PURPLE strip.Color(148, 0, 211)
-#define MAGENTA strip.Color(255, 0, 255)
+// Off color for LEDs
 #define OFF strip.Color(0, 0, 0)
 
-int ledStateIndex = 0;
+// Coloring stuff from https://arduino.stackexchange.com/a/35769
+const uint8_t lights[360] = {
+  0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 15, 17, 18, 20, 22, 24, 26, 28, 30, 32, 35, 37, 39,
+  42, 44, 47, 49, 52, 55, 58, 60, 63, 66, 69, 72, 75, 78, 81, 85, 88, 91, 94, 97, 101, 104, 107, 111, 114, 117, 121, 124, 127, 131, 134, 137,
+  141, 144, 147, 150, 154, 157, 160, 163, 167, 170, 173, 176, 179, 182, 185, 188, 191, 194, 197, 200, 202, 205, 208, 210, 213, 215, 217, 220, 222, 224, 226, 229,
+  231, 232, 234, 236, 238, 239, 241, 242, 244, 245, 246, 248, 249, 250, 251, 251, 252, 253, 253, 254, 254, 255, 255, 255, 255, 255, 255, 255, 254, 254, 253, 253,
+  252, 251, 251, 250, 249, 248, 246, 245, 244, 242, 241, 239, 238, 236, 234, 232, 231, 229, 226, 224, 222, 220, 217, 215, 213, 210, 208, 205, 202, 200, 197, 194,
+  191, 188, 185, 182, 179, 176, 173, 170, 167, 163, 160, 157, 154, 150, 147, 144, 141, 137, 134, 131, 127, 124, 121, 117, 114, 111, 107, 104, 101, 97, 94, 91,
+  88, 85, 81, 78, 75, 72, 69, 66, 63, 60, 58, 55, 52, 49, 47, 44, 42, 39, 37, 35, 32, 30, 28, 26, 24, 22, 20, 18, 17, 15, 13, 12,
+  11, 9, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
 int buttonState = 0;
-int lastButtonState = 0;
-int ledStateIndexMax = 7;
+float downCount = 0;
+bool stateChange = false;
+int r = 0;
+int g = 0;
+int b = 0;
+int rgb_stepper = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   strip.begin();
-  strip.setBrightness(100); //adjust brightness here
-    
+  strip.setBrightness(100);
 }
 
-void loop() { 
+void loop() {
   buttonState = digitalRead(2);
   Serial.print(buttonState);
-  Serial.print(ledStateIndex);
-  if(buttonState == HIGH){
-    switch(ledStateIndex){
-      case 0:
-        setAllColor(OFF);
-        break;
-      case 1:
-        setAllColor(RED);
-        break;
-      case 2:
-        setAllColor(ORANGE);
-        break;
-      case 3:
-        setAllColor(YELLOW);
-        break;
-      case 4:
-        setAllColor(GREEN);
-        break;
-      case 5:
-        setAllColor(BLUE);
-        break;
-      case 6:
-        setAllColor(PURPLE);
-        break;
-      case 7:
-        setAllColor(MAGENTA);
-        break;
-     }
-     if(ledStateIndex >= ledStateIndexMax)
-      ledStateIndex = 0; 
-     lastButtonState = buttonState; 
+  if (buttonState == HIGH && (stateChange == true)) { // IF NOT DOWN
+    if (downCount <= 10000 && downCount > 0) {
+      setAllColor(strip.Color(lights[(rgb_stepper + 120) % 360], lights[rgb_stepper], lights[(rgb_stepper + 240) % 360]));
+      Keyboard.write(KEY_LEFT_GUI);
+      delay(250);
+      setAllColor(OFF);
     }
-    else{
-      if(lastButtonState != buttonState){
-        ledStateIndex++;
-        delay(500);
-      }
-      lastButtonState = buttonState;
-    }
+    stateChange = false;
+    downCount = 0;
   }
+  else if (buttonState == LOW) { // IF DOWN
+    downCount += 1;
+    if (downCount >= 10000) {
+      delay(10);
+      rgb_stepper += 1;
+      setAllColor(strip.Color(lights[(rgb_stepper + 120) % 360], lights[rgb_stepper], lights[(rgb_stepper + 240) % 360]));
+      if (rgb_stepper == 360) {
+        rgb_stepper = 0;
+      }
+    }
+    stateChange = true;
+  }
+}
 
 void setAllColor(uint32_t c) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
   }
-   strip.show();
+  strip.show();
 }
